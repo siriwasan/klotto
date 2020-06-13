a84a923439d3bab86e9039aeca24709ff316a6b2
Siriwasan <siriwasan@gmail.com>
2020-06-13 16:17:54 +0700

    test lib model

--- apps/klotto-mana/src/app/home/home.page.ts
+++ apps/klotto-mana/src/app/home/home.page.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 
+import { Ticket } from '../../../../../lib/models/ticket';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  ticket: Ticket;
 
   constructor() {}
-
 }
