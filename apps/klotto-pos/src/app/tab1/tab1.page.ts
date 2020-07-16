378374f14dc016ac0a86c533c09c4305e4ca438d
Siriwasan <siriwasan@gmail.com>
2020-07-16 14:27:04 +0700

    POS: implement camera

--- apps/klotto-pos/src/app/tab1/tab1.page.ts
+++ apps/klotto-pos/src/app/tab1/tab1.page.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 
+import { Plugins, CameraResultType, CameraSource } from '@capacitor/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  photo: SafeResourceUrl;
 
-  constructor() {}
+  constructor(private sanitizer: DomSanitizer) {}
 
+  async takePicture() {
+    const image = await Plugins.Camera.getPhoto({
+      quality: 100,
+      allowEditing: false,
+      resultType: CameraResultType.DataUrl,
+      source: CameraSource.Camera,
+    });
+
+    this.photo = this.sanitizer.bypassSecurityTrustResourceUrl(
+      image && image.dataUrl
+    );
+  }
 }
